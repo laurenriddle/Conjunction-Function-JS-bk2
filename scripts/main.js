e95a3a2bf7c70dc390a7af892845ae0ca19bf17e
Lauren Riddle <lari7132@gmail.com>
2019-10-15 11:17:01 -0500

    fixed the js code for the conjunction function exercise

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+// console.log("Test Log!")
+
+
+
+
+// Conjunction Function
+let conjoinedWord;
+const conjunction = function (firstWord, secondWord) {
+    conjoinedWord = `${firstWord} ${secondWord}`
+}
+
+conjunction("Master", "Card")
+
+console.log(conjoinedWord)
